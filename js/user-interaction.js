d837c1cb23e4b5ba43210f46684dfe07e3b96bda
Brian <brian.webb8@yahoo.com>
2016-11-09 18:40:03 -0600

    Adding lessons

---
+++ js/user-interaction.js
@@ -0,0 +1,17 @@
+"use strict";
+
+// TODO: Ask the user for their name.
+var response = prompt ("What is your name?")
+
+//       Keep asking if an empty input is provided.
+
+// TODO: Show an alert message that welcomes the user based on their input.
+alert("Hi there " + response + " !");
+
+// TODO: Ask the user if they like pizza.
+//       Based on their answer show a creative alert message.
+
+var liked = confirm("Did you like the pizza?");
+
+(liked)? alert("Good!"): alert("noo!");
+
